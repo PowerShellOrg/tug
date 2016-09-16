88142017e9b40bbc23ae94253bc4b85b0bd8fe8f
concentrateddon <don-jones@pluralsight.com>
2016-09-16 11:22:39 -0700

    Added reference assembly dependencies

--- tug/Startup.cs
+++ tug/Startup.cs
@@ -12,6 +12,8 @@ using Microsoft.AspNetCore.Routing.Template;
 using System.Security.Cryptography;
 using System.Text;
 using System.IO;
+using Microsoft.Extensions.Configuration;
+using System.Management.Automation;
 
 namespace tug
 {
@@ -27,15 +29,34 @@ namespace tug
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
-            loggerFactory.AddConsole(LogLevel.Debug);
+
+            // get configuration
+            var builder = new ConfigurationBuilder();
+            builder.SetBasePath(Directory.GetCurrentDirectory());
+            builder.AddJsonFile("appsettings.json");
+            var config = builder.Build();
+
+            // set up console logging
+            // TODO it would be nice to also have a text file logger
+            if (config["LogType"] == "console" | config["LogType"] == "both") {
+                if (config["DebugLog"] == "true") {
+                    loggerFactory.AddConsole(LogLevel.Debug);
+                } else {
+                    loggerFactory.AddConsole(LogLevel.Information);
+                }
+            }
+
+            // initiate logging
             var logger = loggerFactory.CreateLogger("Tug");
             logger.LogInformation("Tug begins.");
 
+            // set development option
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
 
+            // begin registering routes for incoming requests
             var routeBuilder = new RouteBuilder(app);
 
             // Node registration
@@ -45,25 +66,26 @@ namespace tug
 
                         Authorization HTTP header must match the HTTP body,
                         passed through a SHA-256 digest hash, 
-                        encoded in Base64, and a newline added.
-                        That then gets a second newline, 
+                        encoded in Base64.
+                        That then gets a newline, 
                         the x-ms-date HTTP header from the request, 
                         and is then run through a 
                         SHA-256 digest hash that uses a known RegistrationKey as an HMAC, 
-                        with the result Base64 encoded and a newline added.
+                        with the result Base64 encoded.
 
                         This essentially is a digital signature and proof that the node knows a shared secret registration key.
 
-                        So it's Authorization: Shared xxxxxxx\r\n
+                        So the received header is Authorization: Shared xxxxxxx\r\n
 
                     */
-                    logger.LogDebug("\n\n\n----------------------- PUT: Node registration");
+                    logger.LogInformation("\n\n\nPUT: Node registration");
                     string AgentId = context.GetRouteData().Values["AgentId"].ToString();
                     string Body = new StreamReader(context.Request.Body).ReadToEnd();
                     var Headers = context.Request.Headers;
                     logger.LogDebug("AgentId {AgentId}, Request Body {Body}, Headers {Headers}",AgentId,Body,Headers);
 
                     // get needed headers
+                    // TODO: should check for these existing rather than assuming, fail if they don't
                     string xmsdate = context.Request.Headers["x-ms-date"];
                     string authorization = context.Request.Headers["Authorization"];
                     logger.LogDebug("x-ms-date {date}, Authorization {auth}",xmsdate,authorization);
@@ -84,7 +106,9 @@ namespace tug
                     // and then compare each one
                     string[] registrationKeys = {"91E51A37-B59F-11E5-9C04-14109FD663AE"};
                     
-                    // go through valid registration keys
+                    // go through valid registration keys and create a final signature
+                    // we do this because we might have multiple registration keys, so we
+                    // have to try them all until we find one that matches
                     bool Valid = false;
                     foreach (string key in registrationKeys) {
                         logger.LogDebug("Trying registration key {key}",key);
@@ -112,9 +136,13 @@ namespace tug
                         }
                     }
 
+                    // Because this is a PUT, we're only expected to return an HTTP status code
+                    // TODO we also need to call Set-TugNodeRegistration if the node was valid
                     if (Valid) {
+                        // TODO return HTTP 200
                         return context.Response.WriteAsync($"Registering node {AgentId}");
                     } else {
+                        // TODO return HTTP 404(? check spec)
                         return context.Response.WriteAsync($"Registering node {AgentId}");
                     }
                 }
@@ -123,9 +151,9 @@ namespace tug
             // DSC Action
             routeBuilder.MapPost("Nodes(AgentId={AgentId})/DscAction", context =>
                 {
-                    logger.LogInformation("POST: DSC action request");
-                    var AgentId = context.GetRouteData().Values["AgentId"];
-                    var Body = context.Request.Body;
+                    logger.LogInformation("\n\n\nPOST: DSC action request");
+                    string AgentId = context.GetRouteData().Values["AgentId"].ToString();
+                    string Body = new StreamReader(context.Request.Body).ReadToEnd();
                     var Headers = context.Request.Headers;
                     logger.LogDebug("AgentId {AgentId}, Request Body {Body}, Headers {Headers}",AgentId,Body,Headers);
                     return context.Response.WriteAsync($"DSC action for node {AgentId}");
@@ -135,23 +163,24 @@ namespace tug
             // Asking for a MOF
             routeBuilder.MapPost("Nodes(AgentId={AgentId})/Configurations(ConfigurationName={ConfigurationName})/ConfigurationContent", context =>
                 {
-                    logger.LogInformation("POST: MOF request");
-                    var AgentId = context.GetRouteData().Values["AgentId"];
-                    var ConfigurationName = context.GetRouteData().Values["ConfigurationName"];
-                    var Body = context.Request.Body;
+                    logger.LogInformation("\n\n\nPOST: MOF request");
+                    string AgentId = context.GetRouteData().Values["AgentId"].ToString();
+                    string ConfigurationName = context.GetRouteData().Values["ConfigurationName"].ToString();
+                    string Body = new StreamReader(context.Request.Body).ReadToEnd();
                     var Headers = context.Request.Headers;
                     logger.LogDebug("AgentId {AgentId}, Configuration {Config}, Request Body {Body}, Headers {Headers}",AgentId,ConfigurationName,Body,Headers);
                     return context.Response.WriteAsync($"Request from node {AgentId} for configuration {ConfigurationName}");
                 }
             );
 
+
             // Asking for a module
             routeBuilder.MapPost("Modules(ModuleName={ModuleName},ModuleVersion={ModuleVersion})/ModuleContent", context =>
                 {
-                    logger.LogInformation("POST: Module request");
-                    var ModuleName = context.GetRouteData().Values["ModuleName"];
-                    var ModuleVersion = context.GetRouteData().Values["ModuleVersion"];
-                    var Body = context.Request.Body;
+                    logger.LogInformation("\n\n\nPOST: Module request");
+                    string ModuleName = context.GetRouteData().Values["ModuleName"].ToString();
+                    string ModuleVersion = context.GetRouteData().Values["ModuleVersion"].ToString();
+                    string Body = new StreamReader(context.Request.Body).ReadToEnd();
                     var Headers = context.Request.Headers;
                     logger.LogDebug("Module name {ModuleName}, Version {Version}, Request Body {Body}, Headers {Headers}",ModuleName,ModuleVersion,Body,Headers);
                     return context.Response.WriteAsync($"Module request for {ModuleName} version {ModuleVersion}");
@@ -161,16 +190,16 @@ namespace tug
             // Sending a report
             routeBuilder.MapPost("Nodes(AgentId={AgentId})/SendReport", context =>
                 {
-                    //logger.LogInformation("POST: Report delivery");
-                    var AgentId = context.GetRouteData().Values["AgentId"];
-                    var Body = context.Request.Body;
+                    logger.LogInformation("\n\n\nPOST: Report delivery");
+                    string AgentId = context.GetRouteData().Values["AgentId"].ToString();
+                    string Body = new StreamReader(context.Request.Body).ReadToEnd();
                     var Headers = context.Request.Headers;
-                    //logger.LogDebug("AgentId {AgentId}, Request Body {Body}, Headers {Headers}",AgentId,Body,Headers);
+                    logger.LogDebug("AgentId {AgentId}, Request Body {Body}, Headers {Headers}",AgentId,Body,Headers);
                     return context.Response.WriteAsync($"Report from node {AgentId}");
                 }
             );
 
-
+            // with everything defined, kick it off
             var routes = routeBuilder.Build();
             app.UseRouter(routes);
 
