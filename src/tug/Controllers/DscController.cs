f083d2a78e4a59607ad4bde2d5f83e388a13d6dc
Eugene Bekker <ebekker@powervision.com>
2016-11-29 19:21:02 -0500

    Adding controllers for handling DSC operations
    Split out the operations between:
    * DSC Core operations
    * DSC Reporting operations

---
+++ src/tug/Controllers/DscController.cs
@@ -0,0 +1,129 @@
+using System.IO;
+using System.Security.Cryptography;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using tug.Messages;
+
+namespace tug.Controllers
+{
+    /// <summary>
+    /// A controller that implements the core v2 requests for a
+    /// DSC Pull Server, including registration, status checking,
+    /// configuration retrieval and module retrieval. 
+    /// </summary>
+    public class DscController : Controller
+    {
+        private IDscHandler _dscHandler;
+
+        public DscController(IDscHandler handler)
+        {
+            _dscHandler = handler;
+        }
+
+        [HttpPut]
+        [Route("Nodes(AgentId={AgentId})")]
+        //TODO:  [Authorize]
+        public IActionResult RegisterDscAgent(RegisterDscAgentRequest input)
+        {
+            if (ModelState.IsValid)
+            {
+                _dscHandler.RegisterDscAgent(input.AgentId, input);
+
+                return Json(new
+                {
+                    AgentId = input.AgentId,
+                    Message = "Woohoo!  You're in the 'in' crowd now!",
+                });
+            }
+
+            return base.BadRequest(ModelState);
+        }
+
+        [HttpPost]
+        public IActionResult GetDscAction(GetDscActionRequest input)
+        {
+            if (ModelState.IsValid)
+            {
+                return Json(new GetDscActionResponseBody
+                {
+                    NodeStatus = DscActionStatus.OK,
+                });
+            }
+
+            return base.BadRequest(ModelState);
+        }
+
+
+        [HttpGet]
+        [Route("Nodes(AgentId={AgentId})/Configurations(ConfigurationName={ConfigurationName})/ConfigurationContent")]
+        public IActionResult GetConfiguration(GetConfigurationRequest input)
+        {
+            if (ModelState.IsValid)
+            {
+                var s = _dscHandler.GetConfiguration(input.AgentId, input.ConfigurationName, input);
+                if (s == null)
+                    return NotFound();
+
+                using (s)
+                using (var ms = new MemoryStream())
+                {
+                    // TODO: refactor this so we don't have to keep the whole
+                    //       module content in memory and do more efficient streaming
+                    s.CopyTo(ms);
+                    var bytes = ms.ToArray();
+
+                    using (var hash = SHA256.Create())
+                    {
+                        var csum = hash.ComputeHash(bytes);
+                        var csumHex = System.BitConverter.ToString(csum).Replace("-", "");
+
+                        base.Response.Headers.Add(
+                                GetModuleResponse.ChecksumAlgorithmHeader, "SHA-256");
+                        base.Response.Headers.Add(
+                                GetModuleResponse.ChecksumHeader, csumHex);
+                    }
+
+                    return File(bytes, DscContentTypes.OCTET_STREAM);
+                }
+            }
+
+            return BadRequest(ModelState);
+        }
+
+        [HttpGet]
+        [Route("Modules(ModuleName={ModuleName},ModuleVersion={ModuleVersion})/ModuleContent")]
+        public IActionResult GetModule(GetModuleRequest input)
+        {
+            if (ModelState.IsValid)
+            {
+                var s = _dscHandler.GetModule(input.ModuleName, input.ModuleVersion, input);
+                if (s == null)
+                    return NotFound();
+
+                using (s)
+                using (var ms = new MemoryStream())
+                {
+                    // TODO: refactor this so we don't have to keep the whole
+                    //       module content in memory and do more efficient streaming
+                    s.CopyTo(ms);
+                    var bytes = ms.ToArray();
+
+                    using (var hash = SHA256.Create())
+                    {
+                        var csum = hash.ComputeHash(bytes);
+                        var csumHex = System.BitConverter.ToString(csum).Replace("-", "");
+
+                        base.Response.Headers.Add(
+                                GetModuleResponse.ChecksumAlgorithmHeader, "SHA-256");
+                        base.Response.Headers.Add(
+                                GetModuleResponse.ChecksumHeader, csumHex);
+                    }
+
+                    return File(bytes, DscContentTypes.OCTET_STREAM);
+                }
+            }
+
+            return BadRequest(ModelState);
+        }
+    }
+}
---
+++ src/tug/Controllers/DscReportingController.cs
@@ -0,0 +1,38 @@
+using Microsoft.AspNetCore.Mvc;
+using tug.Messages;
+
+namespace tug.Controllers
+{
+    public class DscReportingController : Controller
+    {
+        [HttpPost]
+        [Route("Nodes(AgentID={AgentId})/SendReport")]
+        public IActionResult SendReport(SendReportRequest input)
+        {
+            if (ModelState.IsValid)
+            {
+                // TODO:
+                // persist the report content indexed by the JobId
+                var jobId = input.Body.JobId;
+            }
+
+            return BadRequest(ModelState);
+        }
+
+        [HttpGet]
+        [Route("Nodes(AgentId={AgentId})/Reports(JobId={JobId}))")]
+        public IActionResult GetReports(GetReportsRequest input)
+        {
+            if (ModelState.IsValid)
+            {
+                return Json(new
+                {
+                    JobId = input.JobId,
+                    Message = "Argh!  Here be the report data!",
+                });
+            }
+
+            return BadRequest(ModelState);
+        }
+    }
+}
