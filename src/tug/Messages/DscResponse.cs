e5b5f3896986e96c3ccc2ebd30a318be30432d1e
Eugene Bekker <ebekker@powervision.com>
2016-11-30 00:29:00 -0500

    Adding common response elements

---
+++ src/tug/Messages/DscResponse.cs
@@ -0,0 +1,9 @@
+namespace tug.Messages
+{
+    public static class DscResponse
+    {
+        public const string PROTOCOL_VERSION_HEADER = "ProtocolVersion";
+        public const string PROTOCOL_VERSION_VALUE = "2.0";
+        
+    }
+}
