ddd5f1133eeef165e41251429c7ba2b04ba2add5
Eugene Bekker <ebekker@powervision.com>
2016-12-13 15:16:33 -0500

    Introducing helpers for business-logic specific utilization of provider components

---
+++ src/Tug.Server.Base/Util/ChecksumHelper.cs
@@ -0,0 +1,75 @@
+using System;
+using System.Linq;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Tug.Server.Configuration;
+
+namespace Tug.Server.Util
+{
+    /// <summary>
+    /// Helper class to manage usage of Checksum Algorithm Providers and
+    /// implementation classes as indicated by user configuration settings.
+    /// </summary>
+    public class ChecksumHelper
+    {
+        private ILogger<ChecksumHelper> _logger;
+        private ChecksumSettings _settings;
+        private ChecksumAlgorithmManager _csumManager;
+        private string _defaultName;
+        private IChecksumAlgorithmProvider _defaultProvider;
+
+        public ChecksumHelper(ILogger<ChecksumHelper> logger,
+                IOptions<ChecksumSettings> settings,
+                ChecksumAlgorithmManager csumManager)
+        {
+            _logger = logger;
+            _settings = settings.Value;
+            _csumManager = csumManager;
+
+            Init();
+        }
+
+        public string DefaultAlgorithmName
+        {
+            get { return _defaultName; }
+        }
+
+        public IChecksumAlgorithm GetAlgorithm(string name = null)
+        {
+            if (name == null)
+                name = _defaultName;
+            return _csumManager.GetProvider(name).Produce();
+        }
+
+        private void Init()
+        {
+            // _logger.LogInformation("constructing Checksum Algorithm Provider Manager");
+            // var csumManager = new ChecksumAlgorithmManager();
+            // services.AddSingleton(csumManager);
+
+            _logger.LogInformation("resolved the following Checksum Providers:");
+            foreach (var fpn in _csumManager.FoundProvidersNames)
+                _logger.LogInformation($"  * [{fpn}]");
+
+            _logger.LogInformation("resolving default Checksum Algorithm:");
+            if (!string.IsNullOrEmpty(_settings?.Default))
+            {
+                _defaultName = _settings.Default;
+                _logger.LogInformation("    resolved as [{defaultProviderName}]", _defaultName);
+                _defaultProvider = _csumManager.GetProvider(_settings.Default);
+                if (_defaultProvider == null)
+                    throw new ArgumentException("invalid, missing or unresolved Provider name");
+                // services.AddSingleton<IChecksumAlgorithmProvider>(csumProvider);
+            }
+            else
+            {
+                _logger.LogWarning("    no explicit Default Checksum algorithm specified");
+                var first = _csumManager.FoundProvidersNames.FirstOrDefault();
+                if (string.IsNullOrEmpty(first))
+                    throw new InvalidOperationException("unable to resolve first provider");
+                _logger.LogInformation("    defaulting to first {firstCsum}", first);
+                _defaultName = first;
+            }
+        }
+    }
+}
---
+++ src/Tug.Server.Base/Util/DscHandlerHelper.cs
@@ -0,0 +1,73 @@
+using System;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Tug.Server.Configuration;
+
+namespace Tug.Server.Util
+{
+    public class DscHandlerHelper
+    {
+        private ILogger<DscHandlerHelper> _logger;
+        private HandlerSettings _settings;
+        private DscHandlerManager _dscManager;
+
+        private IDscHandlerProvider _defaultDscProvider;
+        private IDscHandler _defaultDscHandler;
+
+        public DscHandlerHelper(ILogger<DscHandlerHelper> logger,
+                IOptions<HandlerSettings> settings,
+                DscHandlerManager dscManager)
+        {
+            _logger = logger;
+            _settings = settings.Value;
+            _dscManager = dscManager;
+
+            Init();
+        }
+
+        public IDscHandler DefaultHandler
+        {
+            get { return _defaultDscHandler; }
+        }
+
+        private void Init()
+        {
+            // _logger.LogInformation("constructing DSC Handler Provider Manager");
+            // var dscManager = new DscHandlerManager();
+            _logger.LogInformation("resolved the following DSC Handler Providers:");
+            foreach (var fpn in _dscManager.FoundProvidersNames)
+                _logger.LogInformation($"  * [{fpn}]");
+
+            _logger.LogInformation("resolving target Provider");
+            _defaultDscProvider = _dscManager.GetProvider(_settings.Provider);
+            if (_defaultDscProvider == null)
+                throw new ArgumentException("invalid, missing or unresolved Provider name");
+
+            _logger.LogInformation("applying optional DSC Handler parameters");
+            if (_settings.Params?.Count > 0)
+                _defaultDscProvider.SetParameters(_settings.Params);
+
+            _logger.LogInformation("producing DSC Handler");
+            _defaultDscHandler = _defaultDscProvider.Produce();
+            if (_defaultDscHandler == null)
+                throw new InvalidOperationException("failed to construct DSC Handler");
+
+            // services.AddSingleton<IDscHandler>(dscHandler);
+
+
+
+            // _logger.LogInformation($"Resolving DSC Handler Provider for [{settings?.Provider}]");
+            // var handlerProviderType = Type.GetType(settings?.Provider);
+            // if (handlerProviderType == null)
+            //     throw new Exception("Unable to resolve DSC Handler Provider type (is the type specifiied fully?)");
+
+            // services.AddSingleton<DscHandlerConfig>(new DscHandlerConfig
+            // {
+            //     InitParams = settings?.Params,   
+            // });
+
+            // services.AddSingleton(
+            //         typeof(IDscHandlerProvider), handlerProviderType);
+        }
+    }
+}
