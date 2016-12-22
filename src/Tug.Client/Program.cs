914a4e08f81a141cf9686273efeb22d5fb9a24f5
Eugene Bekker <ebekker@powervision.com>
2016-12-22 10:26:15 -0500

    Adding additonal test scenarios in the currently hard-coded client driver

--- src/Tug.Client/Program.cs
+++ src/Tug.Client/Program.cs
@@ -31,6 +31,20 @@ namespace Tug.Client
                     //    http://stackoverflow.com/questions/12378638/how-to-make-system-net-webproxy-to-not-bypass-local-urls
                     ServerUrl = new Uri($"http://{Dns.GetHostName()}:5000"),
                     Proxy = new Util.BasicWebProxy("http://localhost:8888/"),
+                    RegistrationKey = "f65e1a0c-46b0-424c-a6a5-c3701aef32e5",
+                },
+
+                // Cert Info is a *MUST* when using RegKey authorization
+                CertificateInformation = new CertificateInformation
+                {
+                    FriendlyName = "DSC-OaaS Client Authentication",
+                    Issuer = "CN=DSC-OaaS",
+                    NotAfter = DateTime.Now.AddYears(1).ToString("O"),
+                    NotBefore = DateTime.Now.AddMinutes(-10).ToString("O"),
+                    Subject = "CN=DSC-OaaS",
+                    PublicKey = "U3lzdGVtLlNlY3VyaXR5LkNyeXB0b2dyYXBoeS5YNTA5Q2VydGlmaWNhdGVzLlB1YmxpY0tleQ==",
+                    Thumbprint = "8351F16C2B06634279F2C0287B5430452DA1CD94",
+                    Version = 3,
                 }
             };
 
@@ -38,8 +52,19 @@ namespace Tug.Client
             {
                 using (var client = new DscPullClient(clientConfig))
                 {
-                    client.RegisterDscAgentAsync().Wait();
-                    client.GetDscActionAsync().Wait();
+                    //client.RegisterDscAgentAsync().Wait();
+
+                    var configNames = client.GetDscActionAsync().Result?.ToArray();
+                    if (configNames?.Length > 0)
+                    {
+                        Console.WriteLine("We have configs to get:");
+                        foreach(var cn in configNames)
+                        {
+                            Console.WriteLine($"  * Config [{cn}]");
+                            var bytes = client.GetConfiguration(cn).Result;
+                            Console.WriteLine($"    Got config file with [{bytes.Length}] bytes");
+                        }
+                    }
                 }
             }
             catch (AggregateException ex)
