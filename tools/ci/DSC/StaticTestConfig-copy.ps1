7e57a8126eb341449dfb7fd13d78db323a0c9f08
Eugene Bekker <ebekker@powervision.com>
2017-01-02 08:01:53 -0500

    Adding static DSC config, MOF file and checksum to support unit test

---
+++ tools/ci/DSC/StaticTestConfig-copy.ps1
@@ -0,0 +1,15 @@
+﻿<#
+ # Copyright Â© The DevOps Collective, Inc. All rights reserved.
+ # Licnesed under GNU GPL v3. See top-level LICENSE.txt for more details.
+ #>
+
+## The StaticTestConfig DSC configuration is meant to be used
+## exactly as-is, that is the MOF and checksum files are NOT
+## meant to be published/computed, but copied to the target
+## DSC Pull Servers config repo so that we can perform Unit
+## Tests that are validating expected content
+
+. "$PSScriptRoot\DscCommon.ps1"
+
+copy "$PSScriptRoot\StaticTestConfig.mof"          $configPath
+copy "$PSScriptRoot\StaticTestConfig.mof.checksum" $configPath
