6ff72d422078a75020c2e81592c74dd2d1e55011
Eugene Bekker <ebekker@powervision.com>
2017-01-05 13:02:14 -0500

    Adding utility read-only wrapper config

---
+++ src/Tug.Base/Util/ReadOnlyConfigurationSection.cs
@@ -0,0 +1,101 @@
+using System;
+using System.Collections.Generic;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Primitives;
+
+namespace Tug.Util
+{
+    /// <summary>
+    /// A <see cref="IConfigurationSection">configuration section</see> implementation
+    //  that wraps another instance and prevents modifications to the wrapped instance.
+    /// </summary>
+    /// <remarks>
+    /// When wrapping another instance, you can optionally indicate whether write
+    /// attempts generate exceptions or are silently ignored.
+    /// <p>
+    /// Upon first access, nested configuration elements are themselves wrapped in read-only
+    /// implementations before being returned.
+    /// </p>
+    /// </remarks>
+    public class ReadOnlyConfigurationSection : IConfigurationSection
+    {
+        private IConfigurationSection _inner;
+        private bool _throwOnWrite;
+
+        private Dictionary<string, ReadOnlyConfigurationSection> _children;
+
+        /// <param name="throwOnWrite"><c>true</c> by default which indicates
+        ///     exceptions will be thrown for any write attempts</param>
+        public ReadOnlyConfigurationSection(IConfigurationSection inner, bool throwOnWrite = true)
+        {
+            if (inner == null)
+                throw new ArgumentNullException(nameof(inner));
+            _inner = inner;
+            _throwOnWrite = throwOnWrite;
+        }
+
+        public string this[string key]
+        {
+            get { return _inner[key]; }
+            set
+            {
+                if (_throwOnWrite)
+                {
+                    throw new InvalidOperationException(
+                            /*SR*/"Attempt to write a read-only configuration");
+                }
+            }
+        }
+
+        public string Key
+        {
+            get { return _inner.Key; }
+        }
+
+        public string Path
+        {
+            get { return _inner.Path; }
+        }
+
+        public string Value
+        {
+            get { return _inner.Value; }
+            set
+            {
+                if (_throwOnWrite)
+                {
+                    throw new InvalidOperationException(
+                            /*SR*/"Attempt to write a read-only configuration");
+                }
+            }
+        }
+
+        public IConfigurationSection GetSection(string key)
+        {
+            var cs = _inner.GetSection(key);
+            return cs == null ? null : GetReadOnlySection(cs.Key);
+        }
+
+        public IEnumerable<IConfigurationSection> GetChildren()
+        {
+            foreach (var cs in _inner.GetChildren())
+                yield return GetReadOnlySection(cs.Key);
+        }
+
+        public IChangeToken GetReloadToken()
+        {
+            return _inner.GetReloadToken();
+        }
+
+        private ReadOnlyConfigurationSection GetReadOnlySection(string key)
+        {
+            if (_children == null)
+                _children = new Dictionary<string, ReadOnlyConfigurationSection>();
+            
+            if (!_children.ContainsKey(key))
+                _children.Add(key, new ReadOnlyConfigurationSection(_inner.GetSection(key)));
+            
+            return _children[key];
+        }
+    }
+}
