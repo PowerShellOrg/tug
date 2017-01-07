92f5838271e24264e2fab4c37571111253d937f5
Eugene Bekker <ebekker@powervision.com>
2017-01-07 08:27:32 -0500

    Adding extension data support including a convenient accessor

--- src/Tug.Base/Model/AgentInformation.cs
+++ src/Tug.Base/Model/AgentInformation.cs
@@ -3,10 +3,24 @@
  * Licnesed under GNU GPL v3. See top-level LICENSE.txt for more details.
  */
 
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Tug.Util;
+
 namespace Tug.Model
 {
-    public class AgentInformation
+    public class AgentInformation : IExtData
     {
+        [JsonExtensionData]
+        protected IDictionary<string, JToken> _extData = new Dictionary<string, JToken>();
+
+        public object this[string key]
+        {
+            get { return ((IExtData)this).GetExtData(key); }
+            set { ((IExtData)this).SetExtData(key, value); }
+        }
+
         // NOTE:  DO NOT CHANGE THE ORDER OF THESE PROPERTIES!!!
         // Apparently the order of these properties is important
         // to successfully fulfill the RegKey authz requirements
@@ -19,5 +33,10 @@ namespace Tug.Model
 
         public string IPAddress
         { get; set; }
+
+        IDictionary<string, JToken> IExtData.GetExtData()
+        {
+            return _extData;
+        }
     }
 }
