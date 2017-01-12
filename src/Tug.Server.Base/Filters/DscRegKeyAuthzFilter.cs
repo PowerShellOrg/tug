e9b3aa1bdde2ab3d0b7ac2a027ef453748c02f2d
Eugene Bekker <ebekker@powervision.com>
2017-01-12 12:27:45 -0500

    Introducing an alternative RegKey Authz implementation
    * This new variation is implemented in manner that *feels* more correct
    in that it avoids having to deserialize and re-serialize the DSC Request
    body and instead just operates on the actual content stream received in
    the request.
    * We preserve the original implementation as an *alternative*
    implementation in case we ever need to revert back or to test
    compliance.

--- src/Tug.Server.Base/Filters/DscRegKeyAuthzFilter.cs
+++ src/Tug.Server.Base/Filters/DscRegKeyAuthzFilter.cs
@@ -1,9 +1,12 @@
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.IO;
 using System.Linq;
+using System.Net.Http.Headers;
 using System.Security.Cryptography;
 using System.Text;
+using Microsoft.AspNetCore.Http.Headers;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
 using Microsoft.Extensions.Logging;
@@ -26,7 +29,7 @@ namespace Tug.Server.Filters
     /// elements that are processed and available just before invoking the
     /// resolved Controller Action.
     /// </remarks>
-    public class DscRegKeyAuthzFilter : IActionFilter
+    public class DscRegKeyAuthzFilter : IAuthorizationFilter, IActionFilter
     {
         public const string REG_KEY_PATH = "RegistrationKeyPath";
         public const string REG_SAVE_PATH = "RegistrationSavePath";
@@ -34,6 +37,9 @@ namespace Tug.Server.Filters
         public const string REG_KEY_DEFAULT_FILENAME = "RegistrationKeys.txt";
 
         public const string SHARED_AUTHORIZATION_PREFIX = "Shared ";
+
+        private const string HTTP_CONTEXT_ITEM_AGENT_REG_KEY = nameof(DscRegKeyAuthzFilter)
+                + ":AgentRegKey";
 
         private ILogger _logger;
         private AuthzSettings _settings;
@@ -79,44 +85,65 @@ namespace Tug.Server.Filters
                         .WithData(nameof(_regSavePath), _regSavePath);
         }
 
-        public void OnActionExecuting(ActionExecutingContext context)
+        public void OnAuthorization(AuthorizationFilterContext context)
         {
-            var inputArg = context.ActionArguments?.FirstOrDefault();
+            var routeName = context?.ActionDescriptor?.AttributeRouteInfo?.Name;
+            var body = context?.HttpContext?.Request?.Body;
 
-            // Skip this filter if there is not at least one input argument
-            if (inputArg == null)
+            if (RegisterDscAgentRequest.ROUTE_NAME == routeName)
             {
-                if (_logger.IsEnabled(LogLevel.Trace))
-                    _logger.LogTrace("action does not resolve any arguments; SKIPPING");
-                return;
-            }
+                // The Register DSC message is where we try to validate
+                // the request's body against a valid Registration Key
+                // so we're going to pre-compute the hash of the body
+                // early in the request pipeline so that we can make
+                // use of it later on to compute the full HMAC sigs
 
-            var input = inputArg.Value.Value as DscRequest;
-            if (input == null)
-            {
-                if (_logger.IsEnabled(LogLevel.Trace))
-                    _logger.LogTrace("action does not resolve any DscRequest arguments; SKIPPING");
-                return;
-            }
+                if (body == null)
+                {
+                    _logger.LogError("agent registration request did not provide a request body");
+                    context.Result = new BadRequestResult();
+                    return;
+                }
 
-            var isValid = false;
-            var agentId = input.GetAgentId();
-            
-            // We have 2 scenarios to test for...
-
-            // Scenario #1 - a RegisterDscAgent message in which
-            // case we have to validate the request against a valid
-            // RegKey and "remember" the Agent ID for future calls
-            if (input is RegisterDscAgentRequest)
-            {
-                var requ = (RegisterDscAgentRequest)input;
-                var authz = requ.AuthorizationHeader;
-                var xmsdate = requ.MsDateHeader;
+                var authzHeader = (string)context.HttpContext.Request?.Headers[
+                        nameof(HttpRequestHeaders.Authorization)];
+                var msDateHeader = (string)context.HttpContext.Request?.Headers[
+                        DscRequest.X_MS_DATE_HEADER];
 
                 if (_logger.IsEnabled(LogLevel.Debug))
                 {
-                    _logger.LogDebug("received authorization header [{authzHeader}]", authz);
-                    _logger.LogDebug("received x-ms-date header [{msDateHeader}]", xmsdate);
+                    _logger.LogDebug("received authorization header [{authzHeader}]", authzHeader);
+                    _logger.LogDebug("received x-ms-date header [{msDateHeader}]", msDateHeader);
+                }
+                
+                if (string.IsNullOrEmpty(authzHeader))
+                {
+                    _logger.LogError("agent registration request did not provide an authorization header");
+                    context.Result = new UnauthorizedResult();
+                    return;
+                }
+
+                if (string.IsNullOrEmpty(msDateHeader))
+                {
+                    _logger.LogError("agent registration request did not provide an MS Date");
+                    context.Result = new BadRequestResult();
+                    return;
+                }
+
+                // Make sure the MS Date is in the proper format
+                var msDateValue = DateTime.ParseExact(msDateHeader, DscRequest.X_MS_DATE_FORMAT,
+                        CultureInfo.CurrentCulture);
+                // Make sure the MS Date is reasonbly recent -- TODO:  app setting?
+                var msDateEpoch = DateTime.UtcNow;
+                var msDateDiff = msDateEpoch.Subtract(msDateValue);
+                var minTimeSpan = TimeSpan.FromSeconds(-30);
+                var maxTimeSpan = TimeSpan.FromSeconds(30);
+                if (msDateDiff < minTimeSpan || msDateDiff > maxTimeSpan)
+                {
+                    _logger.LogError("agent registration request provided an out-of-range MS Date",
+                            DscRequest.X_MS_DATE_HEADER);
+                    context.Result = new BadRequestResult();
+                    return;
                 }
 
                 // Resolve reg keys from file as non-blank lines after optional comments
@@ -124,44 +151,114 @@ namespace Tug.Server.Filters
                 var regKeys = File.ReadAllLines(_regKeyFilePath)
                         .Select(x => x.Split('#')[0].Trim())
                         .Where(x => x.Length > 0);
-                var bodyJson = JsonConvert.SerializeObject(requ.Body);
-                var bodyBytes = Encoding.UTF8.GetBytes(bodyJson);
-                isValid = ValidateRegKeySignature(authz, xmsdate, regKeys, bodyBytes);
 
-                if (isValid)
+                using (var ms = new MemoryStream())
                 {
-                    // At this point authorization was successful, so let's remember the Agent ID for future calls
-                    var savePath = Path.Combine(_regSavePath, $"{requ.AgentId}.json");
-                    File.WriteAllText(savePath, JsonConvert.SerializeObject(requ.Body));
-                    if (_logger.IsEnabled(LogLevel.Debug))
-                        _logger.LogDebug("saved registration details to [{savePath}]", savePath);
+                    body.CopyTo(ms);
+
+                    var bodyBytes = ms.ToArray();
+                    var agentRegKey = ValidateRegKeySignature(authzHeader, msDateHeader,
+                            regKeys, bodyBytes);
+
+                    if (string.IsNullOrEmpty(agentRegKey))
+                    {
+                        _logger.LogError("agent registration request failed to match any registration key");
+                        context.Result = new UnauthorizedResult();
+                        return;
+                    }
+
+                    // Remember the reg key for later in the request pipeline (see down below)
+                    context.HttpContext.Items[HTTP_CONTEXT_ITEM_AGENT_REG_KEY] = agentRegKey;
+
+                    // Finally, since we *ate* up the body in order to compute the
+                    // signature we need to replace it with another copy, so that
+                    // it can be model-bound before invoking the action method
+                    context.HttpContext.Request.Body = new MemoryStream(bodyBytes);
+                }
+            }
+        }
+
+        public void OnActionExecuting(ActionExecutingContext context)
+        {
+            var routeName = context?.ActionDescriptor?.AttributeRouteInfo?.Name;
+            var input = (context.ActionArguments?.FirstOrDefault())?.Value;
+            var dscRequ = input as DscRequest;
+
+            if (dscRequ == null)
+            {
+                if (_logger.IsEnabled(LogLevel.Trace))
+                    _logger.LogTrace("action does not resolve any DscRequest arguments; SKIPPING");
+                return;
+            }
+
+            var agentId = dscRequ.GetAgentId();
+            if (agentId == null || agentId == Guid.Empty)
+            {
+                _logger.LogError("DSC request Agent ID is missing or invalid");
+                context.Result = new BadRequestResult();
+                return;
+            }
+
+            if (RegisterDscAgentRequest.ROUTE_NAME == routeName)
+            {
+                // The Register DSC message is where we try to validate
+                // the request's body against a valid Registration Key
+                var regRequ = (RegisterDscAgentRequest)dscRequ;
+                var agentRegKey = context.HttpContext.Items[HTTP_CONTEXT_ITEM_AGENT_REG_KEY]
+                        as string;
+
+                if (regRequ == null)
+                {
+                    _logger.LogError("agent registration request is missing expected request input");
+                    context.Result = new BadRequestResult();
+                    return;
+                }
+
+                if (string.IsNullOrEmpty(agentRegKey))
+                {
+                    _logger.LogError("agent registration request did not match any valid registration key");
+                    context.Result = new UnauthorizedResult();
+                    return;
+                }
+
+                // At this point authorization was successful, so let's remember the Agent ID for future calls
+                var regKeySavePath = Path.Combine(_regSavePath, $"{agentId}.regkey");
+                var detailSavePath = Path.Combine(_regSavePath, $"{agentId}.json");
+                File.WriteAllText(regKeySavePath, agentRegKey);
+                File.WriteAllText(detailSavePath, JsonConvert.SerializeObject(regRequ.Body));
+                if (_logger.IsEnabled(LogLevel.Debug))
+                {
+                    _logger.LogDebug("saved registration key to [{savePath}]", regKeySavePath);
+                    _logger.LogDebug("saved registration details to [{savePath}]", detailSavePath);
                 }
             }
             else
             {
-                // In subsequent calls after the initial reg, we just
-                // need to validate that the Agent ID has been registered
+                // For all other requests, if they are valid DSC
+                // messages we just need to validate that they are
+                // associated with a previously registered Agent ID
+                // so validate that the Agent ID has been registered
                 var savePath = Path.Combine(_regSavePath, $"{agentId}.json");
-                isValid = File.Exists(savePath);
+                var isValid = File.Exists(savePath);
 
                 if (_logger.IsEnabled(LogLevel.Debug))
                 {
                     _logger.LogDebug("looking for registration details at [{savePath}]", savePath);
                     _logger.LogDebug("reg key validation for Agent ID [{agentId}] = [{isValid}]", agentId, isValid);
                 }
-            }
 
-            if (!isValid)
-            {
-                _logger.LogWarning("failed RegKey authorization for Agent ID [{agentId}]", agentId);
-                context.Result = new UnauthorizedResult();
+                if (!isValid)
+                {
+                    _logger.LogWarning("failed RegKey authorization for Agent ID [{agentId}]", agentId);
+                    context.Result = new UnauthorizedResult();
+                }
             }
         }
 
         public void OnActionExecuted(ActionExecutedContext context)
         { }
 
-        public static bool ValidateRegKeySignature(string authzHeader, string msDateHeader,
+        public static string ValidateRegKeySignature(string authzHeader, string msDateHeader,
                 IEnumerable<string> regKeys, byte[] bodyBytes)
         {
             if (string.IsNullOrEmpty(authzHeader) || !authzHeader.StartsWith(
@@ -179,22 +276,22 @@ namespace Tug.Server.Filters
                 var toBeSigned = $"{hashB64}\n{msDateHeader}";
                 var toBeSignedBytes = Encoding.UTF8.GetBytes(toBeSigned);
 
-                foreach (var line in regKeys)
+                foreach (var rk in regKeys)
                 {
-                    var regKey = line.Trim();
+                    var regKey = rk.Trim();
                     var regKeyBytes = Encoding.UTF8.GetBytes(regKey);
                     using (var hmac = new HMACSHA256(regKeyBytes))
                     {
                         var sig = Convert.ToBase64String(hmac.ComputeHash(toBeSignedBytes));
                         if (sig == authzHeader)
                         {
-                            return true;
+                            return rk;
                         }
                     }
                 }
             }
-
-            return false;
+            
+            return null;
         }
     }
 }
