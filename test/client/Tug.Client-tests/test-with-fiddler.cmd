671a1abcc39531bf4d2dd615db99563468252a97
Eugene Bekker <ebekker@powervision.com>
2017-01-18 12:36:50 -0500

    Allowing params to be passed in

--- test/client/Tug.Client-tests/test-with-fiddler.cmd
+++ test/client/Tug.Client-tests/test-with-fiddler.cmd
@@ -5,6 +5,6 @@
 @ECHO THIS=%THIS%
 @ECHO THIS_DIR=%THIS_DIR%
 
-dotnet test %THIS_DIR% -- /proxy_url="http://localhost:8888"
+dotnet test %THIS_DIR% %* -- /proxy_url="http://localhost:8888"
 
 @ENDLOCAL
