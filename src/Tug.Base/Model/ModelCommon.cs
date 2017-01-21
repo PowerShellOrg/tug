683b15adbbd7fb35332ba2da9f6f59b9a872ce51
Eugene Bekker <ebekker@powervision.com>
2017-01-21 16:04:33 -0500

    Adding a class of commonly used values
    Also, correcting default order, implying False is the default condition

--- src/Tug.Base/Model/ModelCommon.cs
+++ src/Tug.Base/Model/ModelCommon.cs
@@ -5,6 +5,11 @@
 
 namespace Tug.Model
 {
+    public static class CommonValues
+    {
+        public static readonly string[] EMPTY_STRINGS = new string[0];
+    }
+
     /// <summary>
     /// Defines a collection of constants representing MIME content types
     /// that are in use by the DSCPM protocol specification.
@@ -25,8 +30,8 @@ namespace Tug.Model
     /// </remarks>
     public enum DscTrueFalse
     {
-        True,
         False,
+        True,
     }
 
     /// <summary>
