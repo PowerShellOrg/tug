7f1ed970bdaaa5d48924deff243386c8d71eb4c2
Eugene Bekker <ebekker@powervision.com>
2017-01-22 12:06:33 -0500

    Filling out reports controller

--- src/Tug.Server/Controllers/DscReportingController.cs
+++ src/Tug.Server/Controllers/DscReportingController.cs
@@ -3,6 +3,7 @@
  * Licnesed under GNU GPL v3. See top-level LICENSE.txt for more details.
  */
 
+using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Tug.Messages;
@@ -14,12 +15,14 @@ namespace Tug.Server.Controllers
     {
         private ILogger<DscController> _logger;
         private DscHandlerHelper _dscHelper;
+        private IDscHandler _dscHandler;
 
         public DscReportingController(ILogger<DscController> logger,
                 DscHandlerHelper dscHelper)
         {
             _logger = logger;
             _dscHelper = dscHelper;
+            _dscHandler = _dscHelper.DefaultHandler;
         }
 
         [HttpPost]
@@ -34,30 +37,57 @@ namespace Tug.Server.Controllers
             if (ModelState.IsValid)
             {
                 _logger.LogDebug($"AgentId=[{input.AgentId}]");
-
-                // TODO:
-                // persist the report content indexed by the JobId
-                var jobId = input.Body.JobId;
+                _dscHandler.SendReport(input.AgentId.Value, input.Body);
             }
 
             return BadRequest(ModelState);
         }
 
         [HttpGet]
-        [Route(GetReportsRequest.ROUTE,
-            Name = GetReportsRequest.ROUTE_NAME)]
-        [ActionName(nameof(GetReports))]
-        public IActionResult GetReports(GetReportsRequest input)
+        [Route(GetReportsRequest.ROUTE_SINGLE,
+            Name = GetReportsRequest.ROUTE_SINGLE_NAME)]
+        [ActionName(nameof(GetReportsSingle))]
+        public IActionResult GetReportsSingle(GetReportsRequest input)
         {
             if (_logger.IsEnabled(LogLevel.Trace))
-                _logger.LogTrace($"{nameof(GetReports)}:  {GetReportsRequest.VERB}");
+                _logger.LogTrace($"{nameof(GetReportsSingle)}:  {GetReportsRequest.VERB}");
 
             if (ModelState.IsValid)
             {
-                return Json(new
+                _logger.LogDebug($"AgentId=[{input.AgentId}]");
+                var sr = _dscHandler.GetReports(input.AgentId.Value, input.JobId);
+                
+                return this.Model(new GetReportsSingleResponse
                 {
-                    JobId = input.JobId,
-                    Message = "Argh!  Here be the report data!",
+                    Body = sr.FirstOrDefault(),
+                });
+            }
+
+            return BadRequest(ModelState);
+        }
+
+        [HttpGet]
+        [Route(GetReportsRequest.ROUTE_ALL,
+            Name = GetReportsRequest.ROUTE_ALL_ALT_NAME)]
+        [Route(GetReportsRequest.ROUTE_ALL,
+            Name = GetReportsRequest.ROUTE_ALL_ALT_NAME)]
+        [ActionName(nameof(GetReportsAll))]
+        public IActionResult GetReportsAll(GetReportsRequest input)
+        {
+            if (_logger.IsEnabled(LogLevel.Trace))
+                _logger.LogTrace($"{nameof(GetReportsAll)}:  {GetReportsRequest.VERB}");
+
+            if (ModelState.IsValid)
+            {
+                _logger.LogDebug($"AgentId=[{input.AgentId}]");
+                var sr = _dscHandler.GetReports(input.AgentId.Value, null);
+                
+                return this.Model(new GetReportsAllResponse
+                {
+                    Body = new Model.GetReportsAllResponseBody
+                    {
+                        Value = sr,
+                    },
                 });
             }
 
