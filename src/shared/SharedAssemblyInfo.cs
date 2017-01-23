514a8812ed53df8765d315ecf43c865bcd3d9485
Eugene Bekker <ebekker@powervision.com>
2017-01-23 06:50:25 -0500

    Switching to attribute-based assembly manifest generation
    Instead of project.json-based, we use attribute-based which allows use
    to define a core set of manifest properties in shared files that can be
    pulled into each project and adjusted in unison.

---
+++ src/shared/SharedAssemblyInfo.cs
@@ -0,0 +1,11 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyCompany("github.com/ebekker/ACMESharp")]
+[assembly: AssemblyProduct("ACMESharp")]
+[assembly: AssemblyCopyright("Copyright © 2016 Eugene Bekker. All rights reserved.")]
+[assembly: AssemblyTrademark("")]
---
+++ src/shared/SharedAssemblyVersionInfo.cs
@@ -0,0 +1,29 @@
+﻿using System.Reflection;
+
+// Version information for an assembly consists of the following four values:
+//
+//      M.N.P.B
+//
+//      Major Version (M)
+//      Minor Version (N)
+//      Patch Number  (P)
+//      Build Number  (B)
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion(ASMINFO.VERSION + ".0")]
+[assembly: AssemblyFileVersion(ASMINFO.VERSION + ".0")]
+
+// This is used for the NuSpec version tag replacement
+// and is combined with nuget-specific rev and release
+[assembly: AssemblyInformationalVersion(ASMINFO.VERSION)]
+
+// ReSharper disable once InconsistentNaming
+internal static class ASMINFO
+{
+
+    // DON'T FORGET TO UPDATE APPVEYOR.YML
+    // ReSharper disable once InconsistentNaming
+    public const string VERSION = "0.8.2";
+}
