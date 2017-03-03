e213934e808d576d0b153786b05e5be62602b0a3
Eugene Bekker <ebekker@powervision.com>
2017-03-03 13:08:43 -0500

    Docs

--- src/Tug.Server.FaaS.AwsLambda/Configuration/AppSettings.cs
+++ src/Tug.Server.FaaS.AwsLambda/Configuration/AppSettings.cs
@@ -5,7 +5,17 @@
 
 namespace Tug.Server.FaaS.AwsLambda.Configuration
 {
-    public class AppSettings
+    /// <summary>
+    /// Defines configuration settings that drive the operational
+    /// runtime behavior of the system.
+    /// </summary>
+    /// <remarks>
+    /// App Settings subclass from Host Settings and so they will
+    /// include a superset of configuration sources that also include
+    /// all the original bootstrap settings used to resolve the
+    /// runtime configuration in case those are needed for reference.
+    /// </remarks>
+    public class AppSettings : HostSettings
     {
         /// <summary>
         /// Default prefix used to identify environment variables
