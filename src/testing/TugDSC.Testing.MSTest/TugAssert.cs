69b6ec7596dc7813248278d25fb8760fb359095b
Eugene Bekker <ebekker@users.noreply.github.com>
2018-01-29 13:14:11 -0500

    migrated MSTest extensions library to netstandard2.0
    also relocated this non-test project (it's test-supporting) to the src directory hierarchy

---
+++ src/testing/TugDSC.Testing.MSTest/TugAssert.cs
@@ -0,0 +1,42 @@
+/*
+ * Copyright © The DevOps Collective, Inc. All rights reserved.
+ * Licensed under GNU GPL v3. See top-level LICENSE.txt for more details.
+ */
+
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TugDSC.Testing.MSTest
+{
+    public static class TugAssert
+    {
+        public static void ThrowsExceptionWhen<T>(Func<T, bool> condition,
+                Action action, string message = null)
+            where T : Exception
+        {
+            ThrowsExceptionWhen<T>(condition, action, message, null);
+        }
+
+        public static void ThrowsExceptionWhen<T>(Func<T, bool> condition,
+                Action action, string message, params object[] parameters)
+            where T : Exception
+        {
+            Exception expected = null;
+            try
+            {
+                action();
+            }
+            catch (Exception ex)
+            {
+                expected = ex;
+            }
+
+            Assert.ThrowsException<T>(() =>
+            {
+                if (expected != null) throw expected;
+            }, message, parameters);
+            
+            Assert.IsTrue(condition((T)expected), message, parameters);
+        }
+    }
+}
